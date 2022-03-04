abfdda15acf2a24fc3172545f5175e5f2ad2783e
bellemoli <89358385+bellemoli@users.noreply.github.com>
2022-03-04 21:00:07 +0100

    changes

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+function showmessage() {
+    document.getElementById("result").style.display="block";
+
+}
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal =document.getElementById("principal").value;
+    if(parseInt(principal)<1) {
+        alert("Enter number")
+        document.getElementById("principal").focus();
+        return; }
+  
+var rate=document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+
+var year = new Date().getFullYear()+parseInt(years);
+
+document.getElementById("depositmessage").innerHTML=""+principal;
+document.getElementById("interestmessage").innerHTML=""+rate;
+document.getElementById("amountmessage").innerHTML=""+interest;
+document.getElementById("yearmessage").innerHTML=""+year;
+showmessage();
+
+return false;
 }
         
+function showRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rate;
+}
